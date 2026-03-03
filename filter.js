a2051b4438d40fd229ce0beaf136d922c79407e5
Ashikniloy1030 <ashiqurrhaman10@gmail.com>
2026-03-03 16:23:22 +0600

    module 27 done with practice

---
+++ filter.js
@@ -0,0 +1,8 @@
+const numbers = [2, 5, 6, 9, 84, 4, 13, 3]
+const evenNumbers = numbers.filter(num => num % 2 === 0);
+console.log(evenNumbers);
+
+
+const friends = ['Ashik', 'Niloy', 'Nuttus Khan', 'Boglus Khan'];
+const nFriends = friends.filter(frd => frd[0] === 'N' );
+console.log(nFriends);
